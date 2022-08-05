fcae6cc058cf9cba4a97f6e7b87cf42cbe7c8242
joe_bob_21@hotmail.com <crbailey@isd535.org>
2022-08-05 10:56:54 -0500

    helper fix, index.js controller

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -3,17 +3,19 @@ exports.isLoggedIn = function (req, res, next) {
     const is_auth_route = req.path.match(/register|login/gi);
   
     if (is_auth_route && user_id) {
-      return res.redirect('/');
+      return res.redirect('/index');
     }
   
     next();
   }
 
-  exports.isNotLoggedIn = function (req, res, next) {
+exports.isNotLoggedIn = function (req, res, next) {
     const user_id = req.session.user_id
     const is_main_page = req.path.match(/index/gi)
 
     if (is_main_page && !user_id) {
-        return res.redirect('/signin')
+        return res.redirect('/login')
     }
+
+    next()
   }
