e8955859e12851e8e212b6283dee120457587f8f
joe_bob_21@hotmail.com <crbailey@isd535.org>
2022-08-09 13:57:36 -0500

    pre-database changes

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const connection = new Sequelize('users_db','root','4321',
+const connection = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
